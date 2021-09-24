3eb8c06dff76dbacdfacf9033afc6465a9219f99
DESKTOP-LF77V8L\simon <sisilopes20@gmail.com>
2021-09-24 11:19:36 -0300

    Adiciona autenticação OAuth com github no projeto

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,18 +1,30 @@
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
+import { signIn, signOut, useSession } from "next-auth/client";
 
 import styles from "./styles.module.scss";
-export function SignInButton() {
-  const isUserLoggedIn = true;
 
-  return isUserLoggedIn ? (
+export function SignInButton() {
+  const [session] = useSession();
+
+  console.log(session);
+
+  return session ? (
     <button type="button" className={styles.signInButton}>
       <FaGithub color="#04d361" />
-      Simone Lopes
-      <FiX color="#737380" className={styles.closeIcon} />
+      {session.user.name}
+      <FiX
+        color="#737380"
+        className={styles.closeIcon}
+        onClick={() => signOut()}
+      />
     </button>
   ) : (
-    <button type="button" className={styles.signInButton}>
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signIn("github")}
+    >
       <FaGithub color="#eba417" />
       Sign in with Github
     </button>
