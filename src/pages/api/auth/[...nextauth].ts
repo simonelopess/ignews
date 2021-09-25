b779791bd86b3e5bcb8f2cc3d835015d8a71fcfe
DESKTOP-LF77V8L\simon <sisilopes20@gmail.com>
2021-09-25 09:59:32 -0300

    configuração do fauna de db comunicação com stripe

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,12 +1,32 @@
-import NextAuth from "next-auth"
-import Providers from "next-auth/providers"
+import NextAuth from "next-auth";
+import Providers from "next-auth/providers";
+
+import { query as q } from 'faunadb';
+
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
     providers: [
         Providers.GitHub({
             clientId: process.env.GITHUB_CLIENT_ID,
             clientSecret: process.env.GITHUB_CLIENT_SECRET,
-            scope: 'read:user'
+            scope: ['read:user'],
         }),
     ],
+    callbacks: {
+        signIn: async (user, account, profile) => {
+            try {
+                await fauna.query(
+                    q.Create(q.Collection('users'), {
+                        data: {
+                            email: user.email,
+                        },
+                    })
+                );
+                return true;
+            } catch {
+                return false;
+            }
+        },
+    },
 })
