90172c06de6a308a4c696ef27e4b5526e81535b6
DESKTOP-LF77V8L\simon <sisilopes20@gmail.com>
2021-09-30 21:21:13 -0300

    Consome API de posts

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -3,8 +3,18 @@ import { getPrismicClient } from "../../services/prismic";
 import styles from "./styles.module.scss";
 import Prismic from "@prismicio/client";
 import { GetStaticProps } from "next";
+import { RichText } from "prismic-dom";
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updateAt: string;
+};
+interface PostsProps {
+  posts: Post[];
+}
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -12,33 +22,13 @@ export default function Posts() {
       </Head>
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>Creating a Monorepo with Lerna & Yarn Workspace</strong>
-            <p>
-              Neste post vamos desenvolver uma página web para demonstrar, na
-              prática, a integração de Mapas em uma aplicação com React usando
-              Leaflet.
-            </p>
-          </a>
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>Creating a Monorepo with Lerna & Yarn Workspace</strong>
-            <p>
-              Neste post vamos desenvolver uma página web para demonstrar, na
-              prática, a integração de Mapas em uma aplicação com React usando
-              Leaflet.
-            </p>
-          </a>
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>Creating a Monorepo with Lerna & Yarn Workspace</strong>
-            <p>
-              Neste post vamos desenvolver uma página web para demonstrar, na
-              prática, a integração de Mapas em uma aplicação com React usando
-              Leaflet.
-            </p>
-          </a>
+          {posts.map((post) => (
+            <a href="#" key={post.slug}>
+              <time>{post.updateAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
         </div>
       </main>
     </>
@@ -56,8 +46,26 @@ export const getStaticProps: GetStaticProps = async () => {
     }
   );
 
-  console.log(JSON.stringify(response, null, 2));
+  const posts = response.results.map((post) => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt:
+        post.data.content.find((content) => content.type === "paragraph")
+          ?.text ?? "",
+      updateAt: new Date(post.last_publication_date).toLocaleDateString(
+        "pt-BR",
+        {
+          day: "2-digit",
+          month: "long",
+          year: "numeric",
+        }
+      ),
+    };
+  });
   return {
-    props: {},
+    props: {
+      posts,
+    },
   };
 };
