2694422fcfa8b0bc654b4ecd40aa3678846133db
DESKTOP-LF77V8L\simon <sisilopes20@gmail.com>
2021-09-30 21:31:04 -0300

    Adicionando navegação no menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,6 @@
 import { SignInButton } from "../SignInButton";
 import styles from "./styles.module.scss";
+import Link from "next/link";
 
 export function Header() {
   return (
@@ -7,7 +8,13 @@ export function Header() {
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
-          <a className={styles.active}>Home</a>
+          <Link href="/">
+            <a className={styles.active}>Home</a>
+          </Link>
+          <Link href="/posts" prefetch>
+            <a className={styles.active}>Home</a>
+          </Link>
+
           <a>Posts</a>
         </nav>
         <SignInButton />
