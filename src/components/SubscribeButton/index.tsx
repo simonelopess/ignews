afa1cbff26a229f83ce4ed510f1f773ef3d50557
Simone Lopes <sisilopes20@gmail.com>
2021-10-05 19:51:34 -0300

    Validando assinatura ativa

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,5 @@
 import { useSession, signIn } from "next-auth/client";
+import { useRouter } from "next/router";
 import { api } from "../../services/api";
 import { getStripeJs } from "../../services/stripe-js";
 import styles from "./styles.module.scss";
@@ -9,6 +10,7 @@ interface SubscribeButtonProps {
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
   const [session] = useSession();
+  const router = useRouter();
 
   async function handleSubscribe() {
     if (!session) {
@@ -16,6 +18,10 @@ export function SubscribeButton({ priceId }: SubscribeButtonProps) {
       return;
     }
 
+    if (session.activeSubscription) {
+      router.push("/posts");
+      return;
+    }
     try {
       const response = await api.post("/subscribe");
 
