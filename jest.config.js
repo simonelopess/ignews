36167e692766680342ae5ee0df5a047237d9e18b
Simone Lopes <sisilopes20@gmail.com>
2022-04-09 11:46:37 -0300

    Cria primeiro component

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,11 @@
 module.exports = {
-    testIgnorePatters: ["/node_modules/", "./next/"],
+    testPathIgnorePatterns: ["/node_modules/", "./next/"],
+    //executa antes do jest executa os testes
+    setupFilesAfterEnv: [
+        "<rootDir>/src/tests/setupTests.ts"
+    ],
     transform: {
         "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest"
-    }
+    },
+    testEnvironment: 'jsdom'
 }
