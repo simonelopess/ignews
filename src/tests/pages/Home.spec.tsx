fa3343c3f00f07174cd1363a804ade2d1cf63104
Simone Lopes <sisilopes20@gmail.com>
2022-04-13 21:28:31 -0300

    add teste no retorgetStaticProps

--- src/tests/pages/Home.spec.tsx
+++ src/tests/pages/Home.spec.tsx
@@ -1,6 +1,8 @@
 import { render, screen } from "@testing-library/react";
+import { mocked } from "ts-jest/utils";
 import { debug } from "console";
-import Home from "../../pages";
+import Home, { getStaticProps } from "../../pages";
+import { stripe } from "../../services/stripe";
 
 jest.mock("next/router");
 jest.mock("next-auth/client", () => {
@@ -9,10 +11,35 @@ jest.mock("next-auth/client", () => {
   };
 });
 
+jest.mock("../../services/stripe");
+
 describe("Home page", () => {
   it("renders correctly", () => {
     render(<Home product={{ priceId: "fake-price-id", amount: "R10,00" }} />);
 
     expect(screen.getByText("for R10,00 month")).toBeInTheDocument();
+  });
+
+  //renderiza props de uma funcao assincrona
+
+  it("loads initial data", async () => {
+    const retriveStripePricesMocked = mocked(stripe.prices.retrieve);
+    retriveStripePricesMocked.mockReturnValueOnce({
+      id: "fake-price-id",
+      unit_amount: 1000,
+    } as any);
+
+    const response = await getStaticProps({});
+
+    expect(response).toEqual(
+      expect.objectContaining({
+        props: {
+          product: {
+            priceId: "fake-price-id",
+            amount: "$10.00",
+          },
+        },
+      })
+    );
   });
 });
